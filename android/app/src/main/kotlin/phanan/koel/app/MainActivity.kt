b4c03fe56a80d2c512e5306eb5bf1f472878ca38
Phan An <me@phanan.net>
2021-08-06 11:28:41 +0200

    fix: pressing Back on Android should minimize, not close app

--- android/app/src/main/kotlin/phanan/koel/app/MainActivity.kt
+++ android/app/src/main/kotlin/phanan/koel/app/MainActivity.kt
@@ -1,6 +1,22 @@
 package phanan.koel.app
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "dev.koel.app"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).apply {
+            setMethodCallHandler { method, result ->
+                if (method.method == "minimize") {
+                    moveTaskToBack(true)
+                }
+            }
+        }
+    }
 }
--- lib/ui/screens/main.dart
+++ lib/ui/screens/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:app/providers/audio_provider.dart';
 import 'package:app/ui/screens/home.dart';
 import 'package:app/ui/screens/library.dart';
@@ -10,6 +12,7 @@ import 'package:flutter/cupertino.dart'
         CupertinoTabScaffold,
         CupertinoTabView;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
@@ -45,46 +48,53 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          CupertinoTabScaffold(
-            tabBuilder: (_, index) {
-              return CupertinoTabView(builder: (_) {
-                return _widgetOptions[index];
-              });
-            },
-            tabBar: CupertinoTabBar(
-              backgroundColor: Colors.black12,
-              iconSize: 24,
-              activeColor: Colors.white,
-              border: Border(top: Divider.createBorderSide(context)),
-              items: const <BottomNavigationBarItem>[
-                const BottomNavigationBarItem(
-                  icon: const Icon(CupertinoIcons.house_fill),
-                  label: 'Home',
-                ),
-                const BottomNavigationBarItem(
-                  icon: const Icon(CupertinoIcons.search),
-                  label: 'Search',
-                ),
-                const BottomNavigationBarItem(
-                  icon: const Icon(CupertinoIcons.music_albums_fill),
-                  label: 'Library',
-                ),
-              ],
-              currentIndex: _selectedIndex,
-              onTap: _onItemTapped,
+    return WillPopScope(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            CupertinoTabScaffold(
+              tabBuilder: (_, index) {
+                return CupertinoTabView(builder: (_) {
+                  return _widgetOptions[index];
+                });
+              },
+              tabBar: CupertinoTabBar(
+                backgroundColor: Colors.black12,
+                iconSize: 24,
+                activeColor: Colors.white,
+                border: Border(top: Divider.createBorderSide(context)),
+                items: const <BottomNavigationBarItem>[
+                  const BottomNavigationBarItem(
+                    icon: const Icon(CupertinoIcons.house_fill),
+                    label: 'Home',
+                  ),
+                  const BottomNavigationBarItem(
+                    icon: const Icon(CupertinoIcons.search),
+                    label: 'Search',
+                  ),
+                  const BottomNavigationBarItem(
+                    icon: const Icon(CupertinoIcons.music_albums_fill),
+                    label: 'Library',
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                onTap: _onItemTapped,
+              ),
             ),
-          ),
-          Positioned(
-            // 50 is the standard iOS (10) tab bar height.
-            bottom: 50 + MediaQuery.of(context).padding.bottom,
-            width: MediaQuery.of(context).size.width,
-            child: const FooterPlayerSheet(),
-          ),
-        ],
+            Positioned(
+              // 50 is the standard iOS (10) tab bar height.
+              bottom: 50 + MediaQuery.of(context).padding.bottom,
+              width: MediaQuery.of(context).size.width,
+              child: const FooterPlayerSheet(),
+            ),
+          ],
+        ),
       ),
+      onWillPop: () async {
+        if (!Platform.isAndroid || Navigator.of(context).canPop()) return true;
+        MethodChannel('dev.koel.app').invokeMethod('minimize');
+        return false;
+      },
     );
   }
 }
