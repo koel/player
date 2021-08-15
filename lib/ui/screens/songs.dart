1deb857dbec1d00e48bed2a082f76e9538dbab7c
Phan An <me@phanan.net>
2021-08-15 18:30:11 +0300

    refactor: sorting in All Songs screen

--- lib/ui/screens/songs.dart
+++ lib/ui/screens/songs.dart
@@ -4,22 +4,12 @@ import 'package:app/ui/widgets/app_bar.dart';
 import 'package:app/ui/widgets/bottom_space.dart';
 import 'package:app/ui/widgets/song_list_buttons.dart';
 import 'package:app/ui/widgets/song_row.dart';
+import 'package:app/ui/widgets/sortable_song_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart' hide AppBar;
 import 'package:provider/provider.dart';
 
-enum OrderBy {
-  artist,
-  title,
-  recentlyAdded,
-}
-
-Map<OrderBy, String> sortOptions = {
-  OrderBy.artist: 'Artist',
-  OrderBy.title: 'Song title',
-  OrderBy.recentlyAdded: 'Recently added',
-};
-
+// Keep track of the sort orders between revisits
 OrderBy _currentSortOrder = OrderBy.recentlyAdded;
 
 class SongsScreen extends StatefulWidget {
@@ -32,82 +22,49 @@ class SongsScreen extends StatefulWidget {
 }
 
 class _SongsScreenState extends State<SongsScreen> {
-  late SongProvider songProvider;
-  late List<Song> songs;
-  late OrderBy _sortOrder = _currentSortOrder;
-
-  @override
-  void initState() {
-    super.initState();
-    songProvider = context.read();
-    songs = songProvider.songs;
-  }
-
-  List<Song> sortSongs({required OrderBy orderBy}) {
-    switch (orderBy) {
-      case OrderBy.title:
-        return songs..sort((a, b) => a.title.compareTo(b.title));
-      case OrderBy.artist:
-        return songs..sort((a, b) => a.artist.name.compareTo(b.artist.name));
-      case OrderBy.recentlyAdded:
-        return songs..sort((a, b) => b.createdAt.compareTo(a.createdAt));
-      default:
-        throw Exception('Invalid order.');
-    }
-  }
+  OrderBy _sortOrder = _currentSortOrder;
 
   @override
   Widget build(BuildContext context) {
-    List<Song> sortedSongs = sortSongs(orderBy: _sortOrder);
-
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          AppBar(
-            headingText: 'All songs',
-            actions: [
-              IconButton(
-                onPressed: () {
-                  showCupertinoModalPopup(
-                    context: context,
-                    builder: (BuildContext context) => CupertinoActionSheet(
-                      title: const Text('Sort by'),
-                      actions: sortOptions.entries
-                          .map(
-                            (entry) => CupertinoActionSheetAction(
-                              onPressed: () {
-                                _currentSortOrder = entry.key;
-                                setState(() => _sortOrder = entry.key);
-                                Navigator.pop(context);
-                              },
-                              child: Text(
-                                (entry.key == _currentSortOrder ? '✓ ' : ' ') +
-                                    entry.value,
-                                style: const TextStyle(color: Colors.white70),
-                              ),
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  );
-                },
-                icon: Icon(CupertinoIcons.sort_down),
-              )
-            ],
-            coverImage: songProvider.coverImageStack,
-          ),
-          SliverToBoxAdapter(child: SongListButtons(songs: sortedSongs)),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (_, int index) => SongRow(
-                song: sortedSongs[index],
-                listContext: SongListContext.allSongs,
+      body: Consumer<SongProvider>(
+        builder: (_, provider, __) {
+          List<Song> songs = sortSongs(provider.songs, orderBy: _sortOrder);
+
+          return CustomScrollView(
+            slivers: [
+              AppBar(
+                headingText: 'All songs',
+                actions: [
+                  SortButton(
+                    options: {
+                      OrderBy.artist: 'Artist',
+                      OrderBy.title: 'Song title',
+                      OrderBy.recentlyAdded: 'Recently added',
+                    },
+                    currentOrder: _sortOrder,
+                    onActionSheetActionPressed: (OrderBy order) {
+                      _currentSortOrder = order;
+                      setState(() => _sortOrder = order);
+                    },
+                  ),
+                ],
+                coverImage: provider.coverImageStack,
               ),
-              childCount: sortedSongs.length,
-            ),
-          ),
-          const SliverToBoxAdapter(child: const BottomSpace()),
-        ],
+              SliverToBoxAdapter(child: SongListButtons(songs: songs)),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (_, int index) => SongRow(
+                    song: songs[index],
+                    listContext: SongListContext.allSongs,
+                  ),
+                  childCount: songs.length,
+                ),
+              ),
+              const SliverToBoxAdapter(child: BottomSpace()),
+            ],
+          );
+        },
       ),
     );
   }
