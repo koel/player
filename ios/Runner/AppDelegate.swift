c56647d638979d9c13e95b3039b2e27a7b5dd4da
Phan An <me@phanan.net>
2024-06-07 14:07:25 +0200

    Revert "Enhances App Compatibility: Adds Support for CarPlay (#95)"
    This reverts commit 6a843420c3b329fa58b60c84dabb2b85a78f90f0.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    return true
-}
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
