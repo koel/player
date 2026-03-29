a8ef7a918ac426facae214b475a755b1616337c1
Phan An <me@phanan.net>
2026-03-29 16:36:36 +0200

    Fix Android back navigation using predictive back gestures (#152)

--- lib/ui/screens/main.dart
+++ lib/ui/screens/main.dart
@@ -11,7 +11,6 @@ import 'package:app/ui/widgets/widgets.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
@@ -28,13 +27,24 @@ class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
   var _isOffline = AppState.get('mode', AppMode.online) == AppMode.offline;
 
+  final _navigatorKeys = List.generate(
+    3,
+    (_) => GlobalKey<NavigatorState>(),
+  );
+
   static const List<Widget> _widgetOptions = [
     const HomeScreen(),
     const SearchScreen(),
     const LibraryScreen(),
   ];
 
-  void _onItemTapped(int index) => setState(() => _selectedIndex = index);
+  void _onItemTapped(int index) {
+    if (index == _selectedIndex) {
+      _navigatorKeys[index].currentState?.popUntil((route) => route.isFirst);
+    } else {
+      setState(() => _selectedIndex = index);
+    }
+  }
 
   @override
   void initState() {
@@ -66,73 +76,67 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      child: Scaffold(
-        body: _isOffline
-            ? Stack(
-                children: [
-                  DownloadedScreen(inOfflineMode: true),
-                  Positioned(
-                    bottom: 0,
-                    width: MediaQuery.of(context).size.width,
-                    child: Container(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          const MiniPlayer(),
-                          const ConnectivityInfoBox(),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            : Stack(
-                children: <Widget>[
-                  CupertinoTabScaffold(
-                    tabBuilder: (_, index) {
-                      return CupertinoTabView(
-                          builder: (_) => _widgetOptions[index]);
-                    },
-                    tabBar: CupertinoTabBar(
-                      backgroundColor: Colors.black12,
-                      iconSize: 24,
-                      activeColor: Colors.white,
-                      height: tabBarHeight,
-                      inactiveColor: Colors.white54,
-                      border: Border(top: Divider.createBorderSide(context)),
-                      items: <BottomNavigationBarItem>[
-                        tabBarItem(
-                          title: 'Home',
-                          icon: CupertinoIcons.house_fill,
-                        ),
-                        tabBarItem(
-                          title: 'Search',
-                          icon: CupertinoIcons.search,
-                        ),
-                        tabBarItem(
-                          title: 'Library',
-                          icon: CupertinoIcons.music_albums_fill,
-                        ),
+    return Scaffold(
+      body: _isOffline
+          ? Stack(
+              children: [
+                DownloadedScreen(inOfflineMode: true),
+                Positioned(
+                  bottom: 0,
+                  width: MediaQuery.of(context).size.width,
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const MiniPlayer(),
+                        const ConnectivityInfoBox(),
                       ],
-                      currentIndex: _selectedIndex,
-                      onTap: _onItemTapped,
                     ),
                   ),
-                  Positioned(
-                    bottom:
-                        tabBarHeight + MediaQuery.of(context).padding.bottom,
-                    width: MediaQuery.of(context).size.width,
-                    child: const MiniPlayer(),
+                ),
+              ],
+            )
+          : Stack(
+              children: <Widget>[
+                CupertinoTabScaffold(
+                  tabBuilder: (_, index) {
+                    return CupertinoTabView(
+                        navigatorKey: _navigatorKeys[index],
+                        builder: (_) => _widgetOptions[index]);
+                  },
+                  tabBar: CupertinoTabBar(
+                    backgroundColor: Colors.black12,
+                    iconSize: 24,
+                    activeColor: Colors.white,
+                    height: tabBarHeight,
+                    inactiveColor: Colors.white54,
+                    border: Border(top: Divider.createBorderSide(context)),
+                    items: <BottomNavigationBarItem>[
+                      tabBarItem(
+                        title: 'Home',
+                        icon: CupertinoIcons.house_fill,
+                      ),
+                      tabBarItem(
+                        title: 'Search',
+                        icon: CupertinoIcons.search,
+                      ),
+                      tabBarItem(
+                        title: 'Library',
+                        icon: CupertinoIcons.music_albums_fill,
+                      ),
+                    ],
+                    currentIndex: _selectedIndex,
+                    onTap: _onItemTapped,
                   ),
-                ],
-              ),
-      ),
-      onWillPop: () async {
-        if (!Platform.isAndroid || Navigator.of(context).canPop()) return true;
-        MethodChannel('dev.koel.app').invokeMethod('minimize');
-        return false;
-      },
+                ),
+                Positioned(
+                  bottom:
+                      tabBarHeight + MediaQuery.of(context).padding.bottom,
+                  width: MediaQuery.of(context).size.width,
+                  child: const MiniPlayer(),
+                ),
+              ],
+            ),
     );
   }
 }
